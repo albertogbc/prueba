1a1a34118c68ceca57795db6bbc9fc8323f7dcf2
José Alberto Álvarez Álvarez <alberto.alvarez@GRUPOGBC.local>
2025-04-12 12:54:48 -0600

    first commit

---
+++ src/main/java/jenkins/jenkins/demo/Controller/Controlador.java
@@ -0,0 +1,15 @@
+package jenkins.jenkins.demo.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class Controlador {
+    
+    @GetMapping("")
+    public String getMethodName() {
+        return new String("Hola mundo");
+    }
+    
+}
